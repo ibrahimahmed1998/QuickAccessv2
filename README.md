51cf05cd0b8ff686c848596f08c5b39cacf8c21d
hema <hema.1998.man@gmail.com>
2022-05-18 08:58:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## QuickAccess
+ <li>Help to make shortcuts of your favorite websites</li>  
+ <li>Access it quickly without writing or think in site name</li>
+  
+## How it Work
+ it depend on your links put in it like in next example ,
+ <a href="URL" <img src="../IMG/IMG.Type title="title" ></a>
+ 
+![alt text](https://github.com/ibrahimahmed1998/QuickAccess/blob/main/main_.png)
+<img src="main2.png">
---
+++ js/nav.js
@@ -0,0 +1,41 @@
+const links = ['English','Marketing','007','Programming','Jobs','General'] ; 
+/*******************************/
+const anc = document.createElement("a");
+anc.setAttribute("class","navbar-brand");
+anc.setAttribute("href","index.html");
+const div9 = document.getElementById("div9").appendChild(anc);
+const div1 = document.getElementById("div1");
+/*******************************/
+const b = document.createElement("b");
+b.innerHTML="Quick Access"; 
+div9.appendChild(b);
+/*******************************/
+const div2 = document.createElement("div");
+div2.setAttribute("class","collapse navbar-collapse");
+div2.setAttribute("id","navbarResponsive");
+const element3 = document.getElementById("div1").appendChild(div2);
+/*******************************/
+const ul = document.createElement('ul');
+ul.setAttribute('class','navbar-nav ms-auto');
+ul.setAttribute('id','ul1');
+const element4 = document.getElementById("navbarResponsive").appendChild(ul);
+/*******************************/
+const li = []; for (let i=0;i<links.length;i++){
+    li[i] = document.createElement('li');
+    li[i].setAttribute('class','nav-item');
+    li[i].setAttribute('id','li'+i);
+    document.getElementById("ul1").appendChild(li[i]);}
+/*******************************/         
+const L = []; for (let i=0;i<links.length;i++){
+    L[i] = document.createElement('a');
+    L[i].setAttribute('class','nav-link');
+    L[i].setAttribute('href',links[i]+".html");
+    L[i].innerHTML=links[i];
+    document.getElementById("li"+i).appendChild(L[i]);}
+/*******************************/            
+const linker = document.createElement("link");
+linker.setAttribute("rel","icon");
+linker.setAttribute("type","image/x-icon");
+linker.setAttribute("href","../IMG/icon1.jpg");
+document.getElementById("head").appendChild(linker);
+/*******************************/
