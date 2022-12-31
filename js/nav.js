9ff7457c3705dfe72b61827f4f1b93d053743137
ibrahimahmed1998 <hema.1998.man@gmail.com>
2022-12-31 22:51:40 +0200

    55

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,10 @@
 const links = ["1","2","3","EN_CS","AR_CS","PROGRAMMING","ENGLISH","MARKETING","JOBS"], 
-create_e = (f, g, d, e, c) => { let b = document.createElement(g); return f.appendChild(b), d && (b.className = d),e && (b.textContent = e), c && b.setAttribute(c.name, c.val), b }
+create_e = (root, ele, d, txt, c) => { 
+    let b = document.createElement(ele); return root.appendChild(b),
+    d && (b.className = d),
+    txt && (b.textContent = txt),
+    c && b.setAttribute(c.name, c.val),
+    b }
 
 const body = document.querySelector("body");
  
@@ -7,11 +12,22 @@ const className = "navbar navbar-expand-lg navbar-dark bg-dark fixed-top";
 const nav = create_e(body, "nav", className, null, { name: "id" ,val:"mainNav" } );
 const div1 = create_e(nav, "div", "container px-4", null, { name:"id" , val:"div1" });
    
-const a = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "index.html" }); 
-create_e(a, "b", null, "Quick Access", null), 
+const a = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "https://www.linkedin.com/in/ibrahimahmed1998" }); 
+const yu = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "https://www.youtube.com/feed/subscriptions" }); 
+const git = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "https://github.com/ibrahimahmed1998" }); 
+
+create_e(a,"img", "imgheader", null, {name:"src",val:"../IMG/in.png"}), 
+create_e(a,"img", "imgheader", null, {name:"src",val:"../IMG/youbtube.png"}), 
+create_e(a,"img", "imgheader", null, {name:"src",val:"../IMG/github-min.png"}), 
+
+
 create_e(div1, "div", "collapse navbar-collapse", null, { name: "id", val: "navbarResponsive" }); 
 const element4 = document.querySelector("#navbarResponsive"), 
 ul = create_e(element4, "ul", "navbar-nav ms-auto", null, { name: "id", val: "ul1" }); 
 links.forEach((c, b) => { let d = c[b] = create_e(ul, "li", "nav-item", null, { name: "id", val: "li" + b }); 
 create_e(d, "a", "nav-link", links[b], { name: "href", val: `${links[b]}.html` }) })
  
+
+
+
+  
