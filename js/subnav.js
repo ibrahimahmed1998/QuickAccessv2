8ee32e54dfa1e978ce3fcec0bd593eba7f1fa329
ibrahimahmed1998 <hema.1998.man@gmail.com>
2023-02-14 15:38:20 +0200

    55

--- js/subnav.js
+++ js/subnav.js
@@ -1,13 +1,16 @@
 
-// create_e = (f, g, d, e, c) => { let b = document.createElement(g); return f.appendChild(b), d && (b.className = d),e && (b.textContent = e), c && b.setAttribute(c.name, c.val), b }
-// const body = document.querySelector("#test");
-// const subnav = create_e(body,'nav','navbar navbar-expand-lg navbar-dark bg-dark',null,{name:'id',val:'mainNav'});
-// const div1 = create_e(subnav,"div","container px-4",null,{name:'id',val:'div1'});
-// if(start=1)
-// {
-//     create_e(div1,'a','navbar-brand js-scroll-trigger',"Back",{name:'href',val:'../index.html'});
-// }
-// else{
-//     create_e(div1,'a','navbar-brand js-scroll-trigger',"Back",{name:'href',val:'../index.html'});
-// }
+create_e = (f, g, d, e, c) => { let b = document.createElement(g); return f.appendChild(b), d && (b.className = d),e && (b.textContent = e), c && b.setAttribute(c.name, c.val), b }
+const body = document.querySelector("#test");
+const subnav = create_e(body,'nav','navbar navbar-expand-lg navbar-dark bg-dark',null,{name:'id',val:'mainNav'});
+const div1 = create_e(subnav,"div","container px-4",null,{name:'id',val:'div1'});
+// create_e(div1,'a','navbar-brand js-scroll-trigger',"Back",{name:'href',val:onclick=history.go(-1)});
 
+
+<button onclick="history.back()">Go Back</button>
+
+
+function test()
+{
+
+    history.back();
+}
