cbdebfdc944f9c40955f69a9b29d4c6fdb6ded9b
ibrahimahmed1998 <hema.1998.man@gmail.com>
2023-07-22 17:48:28 +0300

    update

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-const links = ["1","2","3","English","EN_CS","AR_CS","P1","P2","Marketing","Inside","JOBS"], 
+const links = ["1","2","3","English","AR_CS","P1","P2","Inside"], 
 create_e = (root, ele, d, txt, c) => { 
     let b = document.createElement(ele); return root.appendChild(b),
     d && (b.className = d),
@@ -13,14 +13,14 @@ const nav = create_e(body, "nav", className, null, { name: "id" ,val:"mainNav" }
 const div1 = create_e(nav, "div", "container px-4", null, { name:"id" , val:"div1" });
    
 // const a = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "https://www.linkedin.com/in/ibrahimahmed1998" }); 
-const yu = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "https://www.youtube.com/feed/subscriptions" }); 
-const git = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "https://github.com/ibrahimahmed1998" }); 
-const gpt = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "https://chat.openai.com/chat" }); 
+// const yu = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "https://www.youtube.com/feed/subscriptions" }); 
+// const git = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "https://github.com/ibrahimahmed1998" }); 
+// const gpt = create_e(div1, "a", "navbar-brand", null, { name: "href", val: "https://chat.openai.com/chat" }); 
 
 // create_e(a,"img", "imgheader", null, {name:"src",val:"../IMG/in.png"}), 
-create_e(yu,"img", "imgheader", null, {name:"src",val:"../IMG/youbtube.png"}), 
-create_e(git,"img", "imgheader", null, {name:"src",val:"../IMG/github-min.png"}), 
-create_e(gpt,"img", "imgheader", null, {name:"src",val:"../IMG/chatgpt.jpg"}), 
+// create_e(yu,"img", "imgheader", null, {name:"src",val:"../IMG/youbtube.png"}), 
+// create_e(git,"img", "imgheader", null, {name:"src",val:"../IMG/github-min.png"}), 
+// create_e(gpt,"img", "imgheader", null, {name:"src",val:"../IMG/chatgpt.jpg"}), 
 
 
 create_e(div1, "div", "collapse navbar-collapse", null, { name: "id", val: "navbarResponsive" }); 
@@ -29,9 +29,11 @@ ul = create_e(element4, "ul", "navbar-nav ms-auto", null, { name: "id", val: "ul
 
 links.forEach((c, b) => { let d = c[b] = create_e(ul, "li", "nav-item", null, { name: "id", val: "li" + b }); 
 
-if(links[b] =="JOBS" || links[b] =="P1" ||  
-links[b] =="English" ||  links[b] =="Inside" 
-||  links[b] =="AR_CS" )
+if(links[b] =="AR_CS" ||
+      links[b] =="P1" ||  
+ links[b] =="English" ||  
+ links[b] =="Inside" 
+  )
  {
     create_e(d, "a", "nav-link", links[b], { name: "href", val: `${links[b]}.htm` });    
  }
